8cfe3382f02b3106ab6820d18f5cd10cca2f15f6
lkappelmeier <lkappelmeier@smith.edu>
2024-02-16 15:57:28 -0500

    Assignment 3

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,93 @@
+import java.util.Scanner;
+import java.util.Arrays;
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+  Scanner sc = new Scanner(System.in);
+  String userTranscript[] = new String[0];
+  System.out.println("How many iterations of conversation?");
+  userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "How many iterations of conversation?";
+  String rounds = sc.nextLine();
+  userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = rounds;
+  int roundsInt = Integer.parseInt(rounds);
+  System.out.println("Hello! What do you want to talk about?");  
+  userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "Hello! What do you want to talk about?";
+  int i = 0;
+ 
+  while (i < roundsInt) {
+   
+    String userInput = sc.nextLine();
+    userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = userInput;
+    if (userInput.contains("Happy")){
+      System.out.println("Thank you!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+      userTranscript[userTranscript.length - 1] = "Thank you!";
+    } else if(userInput.contains("Thank you")||userInput.contains("thank you")) {
+  System.out.println("You're welcome!");
+  userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "You're welcome!";
+    } else if(userInput.contains("music")){
+      System.out.println("I love music!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "I love music!";
+    } else if(userInput.contains("grade")){
+      System.out.println("Yes! You will get a good grade on assignment 3 in CSC 120!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "Yes! You will get a good grade on assignment 3 in CSC 120!";
+    } else if(userInput.contains("I")) {
+      System.out.println("Do you do that often?");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "Do you do that often?";
+    } else if (userInput.contains("me")){
+    System.out.println("And what do you expect me to do about that?");
+    userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "And what do you expect me to do about that";
+    } else if(userInput.contains("am")){
+      System.out.println("You are doing that a lot these days!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "You are doing that a lot these days!";
+    } else if (userInput.contains("you")){
+      System.out.println("I agree!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "I agree!";
+    } else if(userInput.contains("my")){
+      System.out.println("Yours is very nice, yes!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "Yours is very nice, yes!";
+    } else if (userInput.contains("your")){
+      System.out.println("I am just a program, and therefore do not have possession over things!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "I am just a program, and therefore do not have possession over things!";
+    } else if (i == 1||i == 4){
+      System.out.println("Well, I think that is lovely!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "Well, I think that is lovely";
+    } else if (i==2 || i == 5){
+      System.out.println("How fascinating!");
+      userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "How fascinating!";
+    } else if (i==0 || i==3 || i == 6) {
+        System.out.println("I would love to hear more!");
+        userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "I would love to hear more!";
+    } else {
+        System.out.println("You're still talking?");
+        userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+        userTranscript[userTranscript.length - 1] = "You're still talking?";
+      };
+      i++;
+    };
+  System.out.println("Alrighty! Goodbye!");
+  userTranscript = Arrays.copyOf(userTranscript, userTranscript.length + 1);
+  userTranscript[userTranscript.length - 1] = "Alrighty! Goodbye!";
+  sc.close();
+  for (String arr : userTranscript) {
+    System.out.println(arr);
   }
-}
+};
+};
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+ Sakura & Jo. 
 - Which session(s) of TA / office hours did you attend?
+Wednesday 7-9 (did not have any specific questions so my name wasn't on the board)
 - What are your initial impressions of Java? 
+I was very confused at first, but I think I am coming around to it. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+The class system and objects are similar between the two, but I feel like python may be slightly more intuitive. I feel like I have not done enough in depth work in either to draw proper conclusions yet. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I think the advice given in class about working it out on a whiteboard or something before coding is very helpful, because then you only need to worry about figuring out the syntax. I realized I did not do the find and replace way which I am a bit confused about. 
